e7d2b54629e681af7715941c28387e655f24588c
Christian Shum-Harden <christian.harden@gmail.com>
2018-05-24 15:19:13 -0700

    (refactor) remove dead code

--- src/s1/cmp.js
+++ src/s1/cmp.js
@@ -2,12 +2,6 @@
 import 'core-js/fn/array/find-index';
 import 'core-js/fn/object/keys';
 import 'core-js/fn/array/find';
-// import 'core-js/fn/array/reduce';
-// import 'core-js/fn/array/fill';
-// import 'core-js/fn/array/map';
-// import 'core-js/fn/array/for-each';
-// import 'core-js/fn/array/filter';
-// import 'core-js/fn/array/from';
 
 import cmp from '../loader';
 import {init} from '../lib/init';
